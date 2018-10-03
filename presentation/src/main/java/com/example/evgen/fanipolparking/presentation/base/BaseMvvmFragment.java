b564772c21d0d56ae875ed1dc1c6bdebb4191713
pandas007 <git@github.com:pandas007/JAVA-IT-ac.git>
2018-10-03 10:30:23 +0300

    add BaseMvvmFragment

---
+++ presentation/src/main/java/com/example/evgen/fanipolparking/presentation/base/BaseMvvmFragment.java
@@ -0,0 +1,56 @@
+package com.example.evgen.fanipolparking.presentation.base;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.evgen.fanipolparking.BR;
+
+
+public abstract class BaseMvvmFragment <Binding extends ViewDataBinding, ViewModel extends BaseViewModel> extends Fragment {
+
+    protected Binding binding;
+    protected ViewModel viewModel;
+
+    public abstract int provideLayoutId();
+    public abstract ViewModel provideViewModel();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        viewModel = provideViewModel();
+        binding = DataBindingUtil.inflate(inflater, provideLayoutId(), container, false);
+        binding.setVariable(BR.viewModel, viewModel);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        viewModel.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        viewModel.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        viewModel.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        viewModel.onStart();
+    }
+}
